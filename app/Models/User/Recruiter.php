342b3eafc7182d9a1143302d89f6d0e061b1b4b9
shimaakamalm2-cmd <shimaakamalm2@gmail.com>
2026-03-17 15:11:10 +0200

    inhertance relationship between user and recruiter

--- app/Models/User/Recruiter.php
+++ app/Models/User/Recruiter.php
@@ -2,11 +2,22 @@
 
 namespace App\Models\User;
 use App\Models\Company\Company;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Recruiter extends User
 {
- protected $table = 'recruiter'; 
- public function companies()
+ protected $table = 'recruiter';
+ protected $fillable = ['user_id','title'];
+ public $timestamps = false;
+ //this shows the inhertance relationship between rec and user
+    public function user() :BelongsTo
     {
-        return $this->belongsToMany(Company::class);
+        //the link between them is here
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function companies()
+    {
+        return $this->belongsToMany(Company::class,'recruiter_companies');
     }
 }
--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -3,8 +3,18 @@
 namespace App\Models\User;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Model
 {
  protected $table = 'users'; // existing table name
+
+    public $timestamps = false;
+
+    //basically meand user has one recruiter with this specific id
+    //this simulates inhertance of user for recruiter
+    public function recruiter(): HasOne{
+        return $this->hasOne(Recruiter::class, 'user_id');
+    }
 }
+// 1-4 ,
