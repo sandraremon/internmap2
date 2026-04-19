bdc2c6065d17e9dc25916335aef42bc3ce7f2a09
shimaakamalm2-cmd <shimaakamalm2@gmail.com>
2026-04-19 23:59:21 +0200

    react router config

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,8 +1,7 @@
 import {type RouteConfig, index, route} from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
+     index("routes/home.tsx"),
     route("/login", "routes/login.tsx"),
-    route("/signup", "routes/signup.tsx"),
-    route("/profile", "routes/profile.tsx"),
+    //route("/home", "routes/home.tsx"),
 ] satisfies RouteConfig;
