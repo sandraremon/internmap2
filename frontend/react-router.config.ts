c5fb4debda56f2e2b38e724054a093cb1a77d7f3
sandraremon <123971540+sandraremon@users.noreply.github.com>
2026-04-19 22:55:25 +0200

    setting up front

--- frontend/react-router.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-
-export default {
-    // Config options...
-    // serverBuildTarget: "static", // Uncomment if you want to deploy to static hosting
-    ssr: false, // Set to false if you want a pure client-side app like standard React
-} satisfies Config;
