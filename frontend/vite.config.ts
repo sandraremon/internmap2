3f4e2335de919291dd73b4ff7235ee5ed8e587dd
sandraremon <123971540+sandraremon@users.noreply.github.com>
2026-04-20 17:04:16 +0200

    some configurations 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [reactRouter()],
 });
