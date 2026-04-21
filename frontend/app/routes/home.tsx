ceb89f04b2edbdd74f2d8e55075c2d18db70887b
Ziad <mziad039@gmail.com>
2026-04-22 00:58:00 +0200

    Another Catastrophic failure

---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,13 @@
+import Home from "../WebPages/home";
+
+// export function meta({}: Route.MetaArgs) {
+//     return [
+//         { title: "InternMap" },
+//         { name: "description", content: "Welcome to our 4th semester project" },
+//     ];
+// }
+
+export default function home() {
+
+    return <Home/>;
+}
