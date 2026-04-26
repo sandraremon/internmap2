83abed0531418b14e43ec4e1f52475d5655bd47d
sandraremon <123971540+sandraremon@users.noreply.github.com>
2026-04-26 19:10:47 +0300

    working on application create logic

--- app/Models/Application/Application.php
+++ app/Models/Application/Application.php
@@ -2,32 +2,36 @@
 
 namespace App\Models\Application;
 use App\Models\JobPosting\JobPosting;
+use App\Models\User\Admin;
+use App\Models\User\Recruiter;
 use App\Models\User\Student;
 use App\Models\UserRole;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+
 /**
  * @method static create(array $array)
- * @method createToken(string $string)
- * @property UserRole $role=$role
+ * @method static find(string $id)
  * @property Student $student = $student
- * @property JobPosting $jobPosting = $jobPosting
+ * @property JobPosting $job=$job
+ *
  */
 class Application extends Model
 {
 
     protected $table = 'application';
-    public $timestamps = true;
+    public $timestamps = false;
 
     protected $primaryKey = 'id';
 
-    protected $fillable=['email', 'f_name', 'l_name', 'phone_number'];
+    protected $fillable= ['email', 'f_name', 'l_name', 'phone_number', 'job_id', 'student_id','application_date'];
 
     public function Student(): BelongsTo
     {
         //the link between them is here by belongsTo
         return $this->belongsTo(Student::class, 'id', 'id');
+        // , 'id','id'
     }
 
     public function jobPosting(): BelongsTo
